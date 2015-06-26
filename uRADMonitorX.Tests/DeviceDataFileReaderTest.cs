416da7d9c03cb4242b1078af9f1bba29f06f7934
Cristian Stoica <cristianstoica85@gmail.com>
2015-06-26 21:33:11 +0300

    Splitted DeviceDataReaderTest.cs into two separate files.

--- uRADMonitorX.Tests/DeviceDataFileReaderTest.cs
+++ uRADMonitorX.Tests/DeviceDataFileReaderTest.cs
@@ -5,19 +5,12 @@ using uRADMonitorX.Core.Device;
 namespace uRADMonitorX.Tests {
 
     [TestFixture]
-    public class DeviceDataReaderTest {
-
-        [TestCase("10.10.0.144")]
-        public void DeviceDataHttpReader(String ipAddress) {
-            IDeviceDataReader deviceDataReader = new DeviceDataHttpReader(ipAddress);
-            DeviceData deviceData = deviceDataReader.Read();
-            // TODO: asserts
-        }
+    public class DeviceDataFileReaderTest {
 
         [TestCase(@"..\..\..\Workspace\10.10.0.144.htm", "41000006", 4, 108, 112, "SBM20", 13, "18,96", "35,00", 100764, 377, 45, 235136, 56, "10.10.0.144", "23.239.13.18", "200")]
         [TestCase(@"..\..\..\Workspace\10.10.0.144-no-pressure-sensor.htm", "41000006", 4, 108, 112, "SBM20", 13, "18,96", "35,00", null, 377, 45, 235136, 56, "10.10.0.144", "23.239.13.18", "200")]
         [TestCase(@"..\..\..\Workspace\10.10.0.144-negative-temp.htm", "41000006", 4, 108, 112, "SBM20", 13, "18,96", "-5,00", 100764, 377, 45, 235136, 56, "10.10.0.144", "23.239.13.18", "200")]
-        public void DeviceDataFileReader(String filePath, String deviceId, int deviceType, int hwVersion, int fwVersion, String detector, int radiation, String radiationAverage, String temperature, int? pressure, int voltage, int voltagePercent, int uptime, int wdt, String ip, String serverIp, String serverResponseCode) {
+        public void Read(String filePath, String deviceId, int deviceType, int hwVersion, int fwVersion, String detector, int radiation, String radiationAverage, String temperature, int? pressure, int voltage, int voltagePercent, int uptime, int wdt, String ip, String serverIp, String serverResponseCode) {
             IDeviceDataReader deviceDataReader = new DeviceDataFileReader(filePath);
             DeviceData deviceData = deviceDataReader.Read();
             Assert.AreEqual(deviceId, deviceData.DeviceInformation.DeviceID);
---
+++ uRADMonitorX.Tests/DeviceDataHttpReaderTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using uRADMonitorX.Core.Device;
+
+namespace uRADMonitorX.Tests {
+
+    [TestFixture]
+    public class DeviceDataHttpReaderTest {
+
+        [TestCase("10.10.0.144")]
+        public void Read(String ipAddress) {
+            IDeviceDataReader deviceDataReader = new DeviceDataHttpReader(ipAddress);
+            DeviceData deviceData = deviceDataReader.Read();
+            // TODO: asserts
+        }
+    }
+}
