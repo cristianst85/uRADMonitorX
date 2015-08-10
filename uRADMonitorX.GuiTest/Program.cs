d42e03751228ee38a719bf34a84600f126d203ee
Cristian Stoica <cristianstoica85@gmail.com>
2015-08-10 23:22:30 +0300

    Changed uRADMonitorX.GuiTest/Program.cs to test system tray notifications.

--- uRADMonitorX.GuiTest/Program.cs
+++ uRADMonitorX.GuiTest/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows.Forms;
 using uRADMonitorX.Commons;
 using uRADMonitorX.Commons.Logging;
+using uRADMonitorX.Commons.Networking;
 using uRADMonitorX.Configuration;
 using uRADMonitorX.Core;
 using uRADMonitorX.Core.Device;
@@ -17,32 +19,49 @@ namespace uRADMonitorX.GuiTest {
 
             ISettings settings = new InMemorySettings() {
                 CloseToSystemTray = true,
-                HasPressureSensor = false,
+                HasPressureSensor = true,
                 TemperatureUnitType = TemperatureUnitType.Celsius,
                 RadiationUnitType = RadiationUnitType.Cpm,
                 PollingType = PollingType.FixedInterval,
                 PollingInterval = 1,
                 DeviceIPAddress = "127.0.0.1",
-                IsPollingEnabled = true
+                IsPollingEnabled = true,
+                AreNotificationsEnabled = true,
+                DetectorName = "SBM20",
+                HighTemperatureNotificationValue = 25,
+                TemperatureNotificationUnitType = TemperatureUnitType.Celsius,
+                RadiationNotificationValue = 0,
+                RadiationNotificationUnitType = RadiationUnitType.Cpm
+
             };
 
             ILogger logger = new NullLogger();
 
             ICollection<DeviceReadings> readings = new Collection<DeviceReadings>();
-            readings.Add(new DeviceReadings() { Radiation = 15, RadiationAverage = 15, Temperature = 25, Pressure = 100000, Voltage = 375, VoltagePercent = 50 });
-            readings.Add(new DeviceReadings() { Radiation = 16, RadiationAverage = 15, Temperature = 24, Pressure = 100020, Voltage = 375, VoltagePercent = 50 });
+            readings.Add(new DeviceReadings() { Radiation = 19, RadiationAverage = 15, Temperature = 26, Pressure = 100040, Voltage = 375, VoltagePercent = 50 });
+            readings.Add(new DeviceReadings() { Radiation = 20, RadiationAverage = 15, Temperature = 25, Pressure = 100000, Voltage = 375, VoltagePercent = 50 });
+            readings.Add(new DeviceReadings() { Radiation = 25, RadiationAverage = 15, Temperature = 24, Pressure = 100020, Voltage = 375, VoltagePercent = 50 });
             readings.Add(new DeviceReadings() { Radiation = 17, RadiationAverage = 15, Temperature = 25, Pressure = 100025, Voltage = 375, VoltagePercent = 50 });
             readings.Add(new DeviceReadings() { Radiation = 18, RadiationAverage = 15, Temperature = 26, Pressure = 100030, Voltage = 375, VoltagePercent = 50 });
             readings.Add(new DeviceReadings() { Radiation = 19, RadiationAverage = 15, Temperature = 25, Pressure = 100040, Voltage = 375, VoltagePercent = 50 });
 
             VirtualDevice virtualDevice = new VirtualDevice("10000000", RadiationDetector.SBM20, 112, 108, DeviceType.Type4, settings.DeviceIPAddress, "0.0.0.0", readings);
+            virtualDevice.ServerResponseCode = HttpStatus.OK;
             virtualDevice.Start();
 
             IDeviceDataReaderFactory deviceDataReaderFactory = new DeviceDataVirtualReaderFactory(virtualDevice);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain(deviceDataReaderFactory, settings, logger, true));
+
+            FormMain formMain = new FormMain(deviceDataReaderFactory, settings, logger);
+            formMain.SettingsChangedEventHandler += new SettingsChangedEventHandler(formMain_SettingsChangedEventHandler);
+
+            Application.Run(formMain);
+        }
+
+        private static void formMain_SettingsChangedEventHandler(object sender, SettingsChangedEventArgs e) {
+            Debug.WriteLine("Settings changed.");
         }
     }
 }
--- uRADMonitorX.GuiTest/Properties/AssemblyInfo.cs
+++ uRADMonitorX.GuiTest/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("uRADMonitorX.GuiTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("uRADMonitorX GUI Test")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Cristian Stoica")]
 [assembly: AssemblyProduct("uRADMonitorX.GuiTest")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Cristian Stoica")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
