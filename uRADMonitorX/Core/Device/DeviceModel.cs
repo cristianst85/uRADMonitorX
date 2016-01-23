bdf0a5204be60ba9421b13be0a18f73887947b32
Cristian Stoica <cristianstoica85@gmail.com>
2016-01-23 19:25:36 +0200

    Fixed/updated device models.

--- uRADMonitorX/Core/Device/DeviceModel.cs
+++ uRADMonitorX/Core/Device/DeviceModel.cs
@@ -1,11 +1,15 @@
-﻿
-namespace uRADMonitorX.Core.Device {
-
+﻿namespace uRADMonitorX.Core.Device {
+    /// <summary>
+    /// Source:
+    /// <para>https://github.com/radhoo/uradmonitor_kit1/blob/master/code/misc/models.h</para>
+    /// </summary>
     public enum DeviceModel {
-
         Unknown = 0,
         A = 1,
-        A2 = 2,
-        D = 3
+        B = 2,
+        C = 3,
+        A2 = 4,
+        KIT1 = 5,
+        D = 6
     }
 }
